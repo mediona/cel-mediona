fbe441a56abd39de2cfe0cd933de3649006deb7e
Fernando Ripoll <fernando@giantswarm.io>
2022-10-20 17:23:06 +0200

    Update intro

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,24 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Comunitat Energetica de Mediona (CEL)</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Comunitat Energetica de Mediona (CEL)" />
+        <h3>Preguntes i Respostes</h3>
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+          <b>1. Què és una comunitat energètica local (CEL)?</b>
+            Les CEL’s són organitzacions formades per agrupacions de diferents agents d’un mateix territori que
+          s’uneixen per transformar la manera en
+          què es produeix i s’accedeix a l’energia, generant beneficis
+          mediambientals, econòmics i socials als seus membres i a les zones locals on opera. Tenen capacitat
+          per a distribuir l’energia generada a qualsevol punt de connexió que es trobi dins un radi de 5
+          00 metres
+          de les instal·lacions. Aquestes entitats estan basades en la participació oberta i voluntària i les controlen
+          les persones sòcies o membres que la conformen.
+            </p>
       </main>
 
       <Footer />
