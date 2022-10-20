201fd0f6d731df9f0066851d606cca8bc89d306d
Fernando Ripoll <fernando@giantswarm.io>
2022-10-20 18:19:03 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -43,8 +43,71 @@ l’eficiència energètica i altres serveis com la mobilitat sostenible, telefo
 formacions  i  compres  col·lectives  de  produc
 tes o serveis (d’energia, d’instal·lacions solars, de cotxe
 elèctric, etc.).
-
-
+            </p>
+        <p className="description">
+          <b>4. Quins són els beneficis d’una CEL?</b><br />
+Els  beneficis  d’una  CEL  inclouen  tres  dimensions:  mediambiental,  social  i  econòmica.
+A nivell mediambiental, les CEL contribueixen a redui
+r les emissions de C02, a través de la producció
+d’energia verda i renovable, treballant l’eficiència i reducció de l’ús energètic.
+A  nivell  social,  les  CEL  són  un  mecanisme  per  democratitzar  el  model  energètic,  garantint  l'accés
+universal a l’energia, la
+sobirania  energètica  del  territori  i  la  lluita  contra  la  pobresa  energètica.  Així
+mateix, les CEL empoderen a les persones que hi participen en la presa de decisions sobre el model
+energètic i contribueixen a millorar les relacions de veïnatge i enfortir l
+a comunitat local.
+Finalment,  a  nivell  econòmic  redueixen  l’import mensual  de  la  factura  elèctrica,  i  el  consum  i  la
+producció local de serveis energètics genera riquesa en el territori en el qual opera la CEL.
+            </p>
+        <p className="description">
+          <b>5. L’energia que rebré de la CEL serà 100% renovable?</b><br />
+            Sí que ho serà. Justament les CEL centren el 100% de la seva producció en energies renovables. No
+obstant això, cal tenir present que la demanda d’energia no coberta per la capacitat de producció de la
+CEL serà proveïda pel sistema elèctric gene
+ral a través d’una comercialitzadora, com fins ara.
+            </p>
+        <p className="description">
+          <b>6. Qui pot participar en una CEL?</b><br />
+            En una CEL hi poden intervenir multiplicitat d’actors, cadascun dels quals assumeix la funció que la
+mateixa CEL hagi acordat, en funció del marc competencial i legal
+que es pugui adjudicar. Així doncs,
+els principals actors que s’identifiquen són la ciutadania organitzada, les administracions públiques,
+les  empreses  i  organitzacions  socials  i  les  persones  individuals.  A  més,  la  participació  és  oberta  i
+voluntària.
+            </p>
+        <p className="description">
+          <b>7. Com puc participar en la CEL de Mediona?</b><br />
+            Existeixen  diferents  maneres  de  participar  en  una  CEL.  En  primer  lloc,  com  a  sòcia  consumidora,
+participant en la producció i/o consum d'energia a canvi de l’aportació d’una quota econòmica per
+cobrir  les  despeses  d
+e  la CEL  (manteniment  de  les  instal·lacions,  despeses administratives, etc.).  En
+segon  lloc,  com  a  sòcia  col·laboradora,  contribuint  en  el  desenvolupament  de  la  CEL  a  través
+d’aportacions de capital, altres recursos i/o en la participació d’activitats de l
+a pròpia CEL. Finalment,
+sense ser sòcia però participant com a difusora o sensibilitzadora de la CEL.
+            </p>
+        <p className="description">
+          <b>8. Quins són els drets de les persones consumidores?</b><br />
+            El primer dret és el de participar en les preses de decisions sobre el present i el futur de la Co
+munitat
+Energètica i el seu model energètic a través dels espais democràtics que s’estableixen en el si de
+l’entitat i que venen regulats per la fórmula jurídica que adopta l’entitat. Aquesta participació pot ser a
+través  de  la  gestió  i  representació  activ
+a  de  la  Comunitat  (en  el  Consell  Rector,  en  cas  de  si  és  una
+cooperativa,  o  Junta  Directiva  si  és  una  associació)  i/o  a  través  de  la  validació  de  les  decisions  que
+prenguin els primers en Assemblea on cada una de les sòcies tindrà un vot.
+El  segon  dret  és
+el  de  beneficiar
+-
+se  dels  diferents  serveis  energètics  que  pot  oferir  la  Comunitat
+Energètica generant un estalvi econòmic en l’ús d’energia. Els principals serveis són els relatius a la
+producció d’energia a través de diferents tecnologies renovables (fot
+ovoltaica, xarxa de calor, biogàs,
+etc.),  l’assessorament  entorn a  l’eficiència  energètica  i altres  serveis  com  la  mobilitat sostenible,
+telefonia mòbil, internet compartit, formacions i compres col·lectives de productes o serveis.
+Finalment, tenen el dret
+d’assistir a les reunions de les Assemblees Generals i dels altres òrgans socials
+als quals siguin convocades i participar en les activitats de formació i d’intercooperació.
             </p>
       </main>
 
