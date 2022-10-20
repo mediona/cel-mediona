33cb04131765ff7a3eb0418ab7c63f2aab13b733
Fernando Ripoll <fernando@giantswarm.io>
2022-10-20 17:26:56 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,11 +10,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
+      <main >
         <Header title="Comunitat Energetica de Mediona (CEL)" />
         <h3>Preguntes i Respostes</h3>
         <p className="description">
-          <b>1. Què és una comunitat energètica local (CEL)?</b>
+          <b>1. Què és una comunitat energètica local (CEL)?</b><br />
             Les CEL’s són organitzacions formades per agrupacions de diferents agents d’un mateix territori que
           s’uneixen per transformar la manera en
           què es produeix i s’accedeix a l’energia, generant beneficis
@@ -23,6 +23,16 @@ export default function Home() {
           00 metres
           de les instal·lacions. Aquestes entitats estan basades en la participació oberta i voluntària i les controlen
           les persones sòcies o membres que la conformen.
+            </p>
+          <b>2. Què és una Comunitat Energètica Local (CEL) impulsada des de l’Economia Social i
+Solidària (ESS)?
+</b><br />
+            Les CEL impulsades des de l’ESS són entitats amb forma jurídica pròpia (associació o cooperativa)
+sense ànim de lucre. Tenen la voluntat de generar dinàmiques cooperatives que transformen la manera
+de  produir  i  consumir  recursos  energètics
+i  que  generen  un  impacte  transformador  a  nivell  social  i
+mediambiental i local.
+
             </p>
       </main>
 
