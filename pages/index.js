472b89d0c281afa382ceaf1a9ccdeaa981d07d41
pipo02mix <fernando@giantswarm.io>
2022-10-24 18:39:39 +0200

    Clean format

--- pages/index.js
+++ pages/index.js
@@ -19,22 +19,12 @@ export default function Home() {
         <h3>Preguntes i Respostes</h3>
         <p className="description">
           <b>1. Què és una comunitat energètica local (CEL)?</b><br />
-            Les CEL són organitzacions formades per agrupacions de diferents agents d’un mateix territori que s’uneixen per a transformar la manera en
-          què es produeix i s’accedeix a l’energia, generant beneficis
-          mediambientals, econòmics i socials als seus membres i a les zones locals on opera. Tenen capacitat
-          per a distribuir l’energia generada a qualsevol punt de connexió que es trobi dins un radi d’un quilòmetre
-          de les instal·lacions. Aquestes entitats estan basades en la participació oberta i voluntària i les controlen
-          les persones sòcies o membres que la conformen.
+            Les CEL són organitzacions formades per agrupacions de diferents agents d’un mateix territori que s’uneixen per a transformar la manera en què es produeix i s’accedeix a l’energia, generant beneficis mediambientals, econòmics i socials als seus membres i a les zones locals on opera. Tenen capacitat per a distribuir l’energia generada a qualsevol punt de connexió que es trobi dins un radi d’un quilòmetre de les instal·lacions. Aquestes entitats estan basades en la participació oberta i voluntària i les controlen les persones sòcies o membres que la conformen.
             </p>
         <p className="description">
-          <b>2. Què és una Comunitat Energètica Local (CEL) impulsada des de l’Economia Social i
-Solidària (ESS)?
-</b><br />
-            Les CEL impulsades des de l’ESS són entitats amb forma jurídica pròpia (associació o cooperativa)
-sense ànim de lucre. Tenen la voluntat de generar dinàmiques cooperatives que transformen la manera
-de  produir  i  consumir  recursos  energètics
-i  que  generen  un  impacte  transformador  a  escala  social,
-mediambiental i local.
+          <b>2. Què és una Comunitat Energètica Local (CEL) impulsada des de l’Economia Social i Solidària (ESS)?
+            </b><br />
+            Les CEL impulsades des de l’ESS són entitats amb forma jurídica pròpia (associació o cooperativa) sense ànim de lucre. Tenen la voluntat de generar dinàmiques cooperatives que transformen la manera de  produir  i  consumir  recursos  energètics i  que  generen  un  impacte  transformador  a  escala  social, mediambiental i local.
 
             </p>
         <p className="description">
@@ -67,78 +57,36 @@ ral a través d’una comercialitzadora, com fins ara.
         <p className="description">
           <b>6. Qui pot participar en una CEL?</b><br />
             En una CEL hi poden intervenir multiplicitat d’actors, cadascun dels quals assumeix la funció que la
-mateixa CEL hagi acordat, en funció del marc competencial i legal
-que es pugui adjudicar. Així doncs,
-els principals actors que s’identifiquen són la ciutadania organitzada, les administracions públiques,
-les  empreses  i  organitzacions  socials  i  les  persones  individuals.  A  més,  la  participació  és  oberta  i
-voluntària.
+mateixa CEL hagi acordat, en funció del marc competencial i legal que es pugui adjudicar. Així doncs, els principals actors que s’identifiquen són la ciutadania organitzada, les administracions públiques, les empreses  i  organitzacions  socials  i  les  persones  individuals.  A  més,  la  participació  és  oberta  i voluntària.
             </p>
         <p className="description">
           <b>7. Com puc participar en la CEL de Mediona?</b><br />
-            Existeixen  diferents  maneres  de  participar  en  una  CEL.  En  primer  lloc,  com  a  sòcia  consumidora,
-participant en la producció i/o consum d'energia a canvi de l’aportació d’una quota econòmica per
-cobrir  les  despeses  d
-e  la CEL  (manteniment  de  les  instal·lacions,  despeses administratives, etc.).  En
-segon  lloc,  com  a  sòcia  col·laboradora,  contribuint  en  el  desenvolupament  de  la  CEL  a  través
-d’aportacions de capital, altres recursos i/o en la participació d’activitats de l
-a pròpia CEL. Finalment,
-sense ser sòcia però participant com a difusora o sensibilitzadora de la CEL.
+            Existeixen  diferents  maneres  de  participar  en  una  CEL.  En  primer  lloc,  com  a  sòcia  consumidora, participant en la producció i/o consum d'energia a canvi de l’aportació d’una quota econòmica per cobrir  les  despeses  de  la CEL  (manteniment  de  les  instal·lacions,  despeses administratives, etc.).  En segon  lloc,  com  a  sòcia  col·laboradora,  contribuint  en  el  desenvolupament  de  la  CEL  a  través d’aportacions de capital, altres recursos i/o en la participació d’activitats de l a pròpia CEL. Finalment, sense ser sòcia però participant com a difusora o sensibilitzadora de la CEL.
             </p>
         <p className="description">
           <b>8. Quins són els drets de les persones consumidores?</b><br />
             El primer dret és el de participar en les preses de decisions sobre el present i el futur de la Co
-munitat
-Energètica i el seu model energètic a través dels espais democràtics que s’estableixen en el si de
-l’entitat i que venen regulats per la fórmula jurídica que adopta l’entitat. Aquesta participació pot ser a
-través  de  la  gestió  i  representació  activ
-a  de  la  Comunitat  (en  el  Consell  Rector,  en  cas  de  si  és  una
-cooperativa,  o  Junta  Directiva  si  és  una  associació)  i/o  a  través  de  la  validació  de  les  decisions  que
-prenguin els primers en Assemblea on cada una de les sòcies tindrà un vot.
-El  segon  dret  és
-el  de  beneficiar
--
-se  dels  diferents  serveis  energètics  que  pot  oferir  la  Comunitat
-Energètica generant un estalvi econòmic en l’ús d’energia. Els principals serveis són els relatius a la
-producció d’energia a través de diferents tecnologies renovables (fot
-ovoltaica, xarxa de calor, biogàs,
-etc.),  l’assessorament  entorn a  l’eficiència  energètica  i altres  serveis  com  la  mobilitat sostenible,
-telefonia mòbil, internet compartit, formacions i compres col·lectives de productes o serveis.
-Finalment, tenen el dret
-d’assistir a les reunions de les Assemblees Generals i dels altres òrgans socials
-als quals siguin convocades i participar en les activitats de formació i d’intercooperació.
+munitat Energètica i el seu model energètic a través dels espais democràtics que s’estableixen en el si de l’entitat i que venen regulats per la fórmula jurídica que adopta l’entitat. Aquesta participació pot ser a través  de  la  gestió  i  representació  activa  de  la  Comunitat  (en  el  Consell  Rector,  en  cas  de  si  és  una cooperativa,  o  Junta  Directiva  si  és  una  associació)  i/o  a  través  de  la  validació  de  les  decisions  que prenguin els primers en Assemblea on cada una de les sòcies tindrà un vot.
+El  segon  dret  és el  de  beneficiarse  dels  diferents  serveis  energètics  que  pot  oferir  la  Comunitat
+Energètica generant un estalvi econòmic en l’ús d’energia. Els principals serveis són els relatius a la producció d’energia a través de diferents tecnologies renovables (fotovoltaica, xarxa de calor, biogàs,
+etc.),  l’assessorament  entorn a  l’eficiència  energètica  i altres  serveis  com  la  mobilitat sostenible, telefonia mòbil, internet compartit, formacions i compres col·lectives de productes o serveis. Finalment, tenen el dret d’assistir a les reunions de les Assemblees Generals i dels altres òrgans socials als quals siguin convocades i participar en les activitats de formació i d’intercooperació.
             </p>
         <p className="description">
           <b>Quines són les obligacions de les persones consumidores?</b><br />
-            Les persones sòcies
-estan obligades a fer el pagament de l’aportació obligatòria al capital social, si és
-una  cooperativa,  així  com  les  altres  aportacions  econòmiques  que  s’estableixin  per  a  l’accés  als
-diferents  serveis  energètics  i  la  gestió  de  la  Comunitat  Energètica  una  v
-egada  ja  estigui  constituïda.
-D’altra  banda,  a  nivell  administratiu,  les  persones  participants  hauran  d’aportar  la  informació
-necessària per tal d’assegurar l’alta i el correcte funcionament dels serveis energètics.
+            Les persones sòcies estan obligades a fer el pagament de l’aportació obligatòria al capital social, si és una  cooperativa,  així  com  les  altres  aportacions  econòmiques  que  s’estableixin  per  a  l’accés  als diferents  serveis  energètics  i  la  gestió  de  la  Comunitat  Energètica  una  v egada  ja  estigui  constituïda. D’altra  banda,  a  nivell  administratiu,  les  persones  participants  hauran  d’aportar  la  informació necessària per tal d’assegurar l’alta i el correcte funcionament dels serveis energètics.
         </p>
         <p className="description">
           <b>10. Quant hauré de pagar per a formarpart de la CEL?</b><br />
-            Hauràs de pagar l’aportació obligatòria al capital social per fer
--
-te soci/a de la cooperativa que s’hagi
-decidit, així  com  les  altres  obligacions  econòmiques  que s’estableixin  en  forma  d’aportacions pel
-funcionament de la CEL i els diferen
-ts serveis que pugui oferir.
+            Hauràs de pagar l’aportació obligatòria al capital social per ferte soci/a de la cooperativa que s’hagi decidit, així  com  les  altres  obligacions  econòmiques  que s’estableixin  en  forma  d’aportacions pel funcionament de la CEL i els diferen ts serveis que pugui oferir.
         </p>
         <p className="description">
           <b>11. En cas de molta demanda, com es prioritzarà qui s’abasteix d’energia de la CEL i qui no?</b><br />
-Els  criteris  els  decidirà  el  Grup  Motor  (abans  de  constituir  la  CEL)  o  el  Consell  Rector  una  vegada
-constituïda  i  hauran  de  ser  vali
-dats  per  l'Assemblea.  Alguns  dels  criteris  que  es  poden  tenir  en
-consideración  són,  per  exemple,  l’ordre  d’entrada,  el  tractament  de  la  pobresa  energètica  o  si
-l’habitatge és primera o segona residència.
+Els  criteris  els  decidirà  el  Grup  Motor  (abans  de  constituir  la  CEL)  o  el  Consell  Rector  una vegada constituïda  i  hauran  de  ser  validats  per  l'Assemblea.  Alguns  dels  criteris  que  es  poden  tenir  en
+consideración  són,  per  exemple,  l’ordre  d’entrada,  el  tractament  de  la  pobresa  energètica  o  si l’habitatge és primera o segona residència.
         </p>
         <p className="description">
           <b>12. Què passa si vull formar part de la CEL però no visc dins el rati de 1000 metres?</b><br />
-            Pots ferte’n soci/a per col·laborar amb al CEL i beneficiarte dels altres serveis que pugui oferir la CEL
-com   ara   assessorament   energètic,   la   mobilitat   sostenible,   telefonia   mòbil,   internet   compartit,
-formacions i compres col·lectives de productes o serveis.
+            Pots ferte’n soci/a per col·laborar amb al CEL i beneficiarte dels altres serveis que pugui oferirla CEL com   ara   assessorament   energètic,   la   mobilitat   sostenible,   telefonia   mòbil, internet   compartit, formacions i compres col·lectives de productes o serveis.
         </p>
         <p className="description">
           <b>13. Podré participar de la CEL i consumir energia si no sóc propietària del pis on visc? I si el contracte de la llum no està a nom de la llogatera?</b><br />
