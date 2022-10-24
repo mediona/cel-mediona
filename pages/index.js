becb486bb994e0dc2a8f63c6735414cbfba0403c
Fernando Ripoll <fernando@giantswarm.io>
2022-10-24 18:16:59 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -104,6 +104,84 @@ Finalment, tenen el dret
 d’assistir a les reunions de les Assemblees Generals i dels altres òrgans socials
 als quals siguin convocades i participar en les activitats de formació i d’intercooperació.
             </p>
+        <p className="description">
+          <b>Quines són les obligacions de les persones consumidores?</b><br />
+            Les persones sòcies
+estan obligades a fer el pagament de l’aportació obligatòria al capital social, si és
+una  cooperativa,  així  com  les  altres  aportacions  econòmiques  que  s’estableixin  per  a  l’accés  als
+diferents  serveis  energètics  i  la  gestió  de  la  Comunitat  Energètica  una  v
+egada  ja  estigui  constituïda.
+D’altra  banda,  a  nivell  administratiu,  les  persones  participants  hauran  d’aportar  la  informació
+necessària per tal d’assegurar l’alta i el correcte funcionament dels serveis energètics.
+        </p>
+        <p className="description">
+          <b>10. Quant hauré de pagar per a formarpart de la CEL?</b><br />
+            Hauràs de pagar l’aportació obligatòria al capital social per fer
+-
+te soci/a de la cooperativa que s’hagi
+decidit, així  com  les  altres  obligacions  econòmiques  que s’estableixin  en  forma  d’aportacions pel
+funcionament de la CEL i els diferen
+ts serveis que pugui oferir.
+        </p>
+        <p className="description">
+          <b>11. En cas de molta demanda, com es prioritzarà qui s’abasteix d’energia de la CEL i qui no?</b><br />
+Els  criteris  els  decidirà  el  Grup  Motor  (abans  de  constituir  la  CEL)  o  el  Consell  Rector  una  vegada
+constituïda  i  hauran  de  ser  vali
+dats  per  l'Assemblea.  Alguns  dels  criteris  que  es  poden  tenir  en
+consideración  són,  per  exemple,  l’ordre  d’entrada,  el  tractament  de  la  pobresa  energètica  o  si
+l’habitatge és primera o segona residència.
+        </p>
+        <p className="description">
+          <b>12. Què passa si vull formar part de la CEL però no visc dins el rati de 1000 metres?</b><br />
+            Pots ferte’n soci/a per col·laborar amb al CEL i beneficiarte dels altres serveis que pugui oferir la CEL
+com   ara   assessorament   energètic,   la   mobilitat   sostenible,   telefonia   mòbil,   internet   compartit,
+formacions i compres col·lectives de productes o serveis.
+        </p>
+        <p className="description">
+          <b>13. Podré participar de la CEL i consumir energia si no sóc propietària del pis on visc? I si el contracte de la llum no està a nom de la llogatera?</b><br />
+Si  no  ets  propietària,  pots  ferte  sòcia  consumidora  o col·laboradora  de  la  CEL.  Ara  bé,  per  ser  sòcia
+consumidora d’energia cal que el contracte de la llum estigui al teu nom.    
+        </p>
+        <p className="description">
+          <b>14. No tinc clar quant de temps viuré a Mediona o a la casa on visc ara. Què passa si participo a la CEL i canvio de domicili?</b><br />
+Si ets sòcia consumidora i ja no estàs dins del radi de 1000 metres al voltant de la instal·lació, deixaràs
+de ser elegible com a sòcia consumidora de la CEL. De totes maneres, podràs seguir participant hi sent sòcia col·laboradora. En el cas que volguessin deixar ne de formar part, les sòcies tenen total llibertat de deixarho de ser i recuperar el capital social inicial aportat.
+        </p>
+        <p className="description">
+          <b>15. Si tinc plaques a casa meva, em podré sumar a la CEL?</b><br />
+            Si tens plaques solars a casa teva, igualment podràs participar de la CEL com a sòcia col·laboradora o
+sòcia productora i consumidora. En el primer cas, contribuint en el desenvolupament de la CEL a través
+d’aportacions de capital, altres recursos i/o en la participació d’activitats de la pròpia CEL. En el segon
+cas, pots contribuir en la producció d’energia dins la CEL i ser una consumidora més. Les condicions en
+les que una persona sòcia esdevé productora d’energia de la CEL vindran definides per la pròpia
+comunitat.
+        </p>
+        <p className="description">
+          <b>16. Com es relaciona la CEL amb la comercialitzadora?</b><br />
+No és indispensable que per participar d’una CEL tothom hagi d’estar amb la mateixa comercialitzadora
+d’electricitat. Pot ser una decisió de la mateixa Comunitat si considera que és un element rellevant
+establir si aquesta condició és necessària o no. Per tant, si així es considera més adequat, cada persona consumidora de la CEL podrà contractar la llum i rebre les factures de la comercialitzadora que vulgui.
+        </p>
+        <p className="description">
+          <b>17. Quines diferències hi ha entre ferte sòcia de Som Energia i constituir una CEL?</b><br />
+Som Energia és una cooperativa productora i comercialitzadora d’electricitat verda. Qui se’n fa soci/a pot contractar la llum, participar en projectes de generació renovable, fer aportacions al capital social, participar activament a la cooperativa i a les formacions de l’Aula Popular.
+Constituir una CEL és impulsar la transició energètica a nivell local, desenvolupant generació distribuïda i renovable en el territori, empoderant la ciutadania i involucrant a les entitats municipals en aquesta transformació. Segurament són dues opcions complementàries perquè, com es diu abans, en principi sempre caldrà el vincle amb una comercialitzadora.
+        </p>
+        <p className="description">
+          <b>18. Com es pot assegurar la participació de famílies en situació de pobresa energètica a la CEL?</b><br />
+Existeixen  diferents  mecanismes  per  assegurar  que  les  famílies  en  situació  de  pobresa  energètica puguin beneficiarse i participar dels diferents serveis energètics que pot oferir la CEL:
+La primera opció és que en els estatuts de l’entitat, s’incorporin diferents criteris i premisses per prioritzar l’accés a les famílies en situació de pobresa energètica als serveis que ofereix la
+CEL.
+La segona  opció  és  que  el propi  Ajuntament  de  Mediona afegeixi  en el  conveni  de  cessió  de l’espai per les instal·lacions clàusules que incorporin diferents criteris i premisses per prioritzar l’accés a aquestes famílies
+En ambdós casos, addicionalment es podrien introduir opcions per facilitar les obligacions econòmiquesde les sòcies participants. Aquestes poden ser rebaixes o exempcions en el pagament de les quotes i/o en les aportacions inicials de capital d’aquestes famílies que haurien de ser compensades per la resta de les sòcies.
+        </p>
+        <p className="description">
+          <b>19. Podré participar de la CEL si rebo el Bo Social? Quines conseqüències té?</b><br />
+Sí,  podràs  formar  part  de  la  CEL  com  a sòcia  i  beneficiarte  dels  diferents  serveis  que  ofereix  la Comunitat. Una de les condicions essencials per rebre el Bo Social és no superar un llindar establert de consum d’energia anual. En la mesura en que al formar part de la CEL com a sòcia consumidor a redueixes el consum d’energia de la xarxa, les famílies que actualment reben el Bo Social continuaran sent elegibles per rebre aquesta prestació.
+A nivell econòmic, el Bo Social s’aplica sobre el total de la factura de la llum (potència contractada i consum). Conseqüentment, degut a que el consum d’energia de la xarxa serà inferior una vegada la participant consumidora rebi energia de la CEL, la prestació del Bo Social serà inferior a la que rebia abans  de  participar  a  la  Comunitat.  Ara  bé,  és  important  tenir  en  compte  que  obtindrà  un  estalvi econòmic procedent del consum d’aquella energia produïda per la CEL.
+Finalment,  com  a  última  consideració,  cal  recordar  que  totes  aquestes  famílies  hauran  de  seguir connectades a la xarxa a través de les comercialitzadores de referència (CUR) per seguir aplicant pel
+Bo Social, fet no incompatible en participar de la CEL.
+        </p>
       </main>
 
       <Footer />
