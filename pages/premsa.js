18f1a52837da4b91349c8322c33cf0544d6fad07
suaratrema <92716947+suaratrema@users.noreply.github.com>
2023-07-05 17:33:08 +0200

    Update premsa.js
    esmenes gramaticals

--- pages/premsa.js
+++ pages/premsa.js
@@ -9,7 +9,7 @@ export default function Home() {
         <li><a href="https://lafurapenedes.cat/comunitats-energetiques/"> La Fura - Comunitats Energetics</a></li>
         <li><a href="https://www.rtvvilafranca.cat/2022/12/el-penedes-planteja-la-necessitat-dabordar-el-debat-sobre-el-model-energetic/"> RTV Vilafranca - El Penedès planteja la necessitat d’abordar el debat sobre el model energètic</a></li>
         <li><a href="https://blog.somenergia.coop/grupos-locales/catalunya/alt-penedes/2022/02/xerrada-a-mediona-comunitats-energetiques-locals-cels/"> Blog Som Energia</a></li>
-        <li><a href="/triptic.pdf" target="_blank">Triptic de la Feria de Entitats</a></li>
+        <li><a href="/triptic.pdf" target="_blank">Tríptic de la Fira d'Entitats</a></li>
       </ul>
     </main>
 
